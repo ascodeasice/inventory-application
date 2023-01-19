0932649ab346f17b2d0f8ebe5687b95a1236abb7
ascodeasice <ab0988956087@gmail.com>
2023-01-19 10:44:15 +0800

    Finish update routes for book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -186,17 +186,16 @@ exports.book_create_post = [
 
                     res.render("book_form", {
                         title: "Create book",
-                        logoURL: "../../images/amasonLogo.png",
+                        logoURL: "../../../images/amasonLogo.png",
                         book: newBook,
                         authors: results.authors,
                         categories: results.categories,
                         publishers: results.publishers,
                         errors: errors.array(),
                     });
-
-                    return;
                 }
-            )
+            );
+            return;
         }
 
         // no errors
@@ -220,11 +219,162 @@ exports.book_delete_post = (req, res) => {
 };
 
 // Display book update form on GET.
-exports.book_update_get = (req, res) => {
-    res.send("NOT IMPLEMENTED: book update GET");
+exports.book_update_get = (req, res, next) => {
+    async.parallel(
+        {
+            book(callback) {
+                Book.findById(req.params.id).exec(callback);
+            },
+            authors(callback) {
+                Author.find({}).sort({ name: 1 }).exec(callback);
+            },
+            categories(callback) {
+                Category.find({}).sort({ name: 1 }).exec(callback);
+            },
+            publishers(callback) {
+                Publisher.find({}).sort({ name: 1 }).exec(callback);
+            },
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+
+            if (results.book == null) {
+                const error = new Error("Book not found");
+                error.status = 404;
+                return next(error);
+            }
+
+            res.render("book_form", {
+                title: "Update book",
+                logoURL: "../../../images/amasonLogo.png",
+                book: results.book,
+                authors: results.authors,
+                categories: results.categories,
+                publishers: results.publishers,
+            });
+        }
+    );
 };
 
 // Handle book update on POST.
-exports.book_update_post = (req, res) => {
-    res.send("NOT IMPLEMENTED: book update POST");
-};
+exports.book_update_post = [
+    body("title", "Title is required")
+        .trim()
+        .isLength({ min: 1 }),
+    body("description", "Description is required")
+        .trim()
+        .isLength({ min: 1 }),
+    body("category", "Category is required")
+        .trim()
+        .isLength({ min: 1 }),
+    body("price")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Price is required")
+        .isFloat({ min: 0 })
+        .withMessage("Price must be positive"),
+    body("numberInStock")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Number in stock is required")
+        .isFloat({ min: 0 })
+        .withMessage("Number in stock must be positive"),
+    body("publisher", "Publisher is required")
+        .trim()
+        .isLength({ min: 1 }),
+    body("isbn", "ISBN is required")
+        .trim()
+        .isLength({ min: 1 }),
+    body("width")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Width is required")
+        .isFloat({ min: 0 })
+        .withMessage("Width must be positive"),
+    body("depth")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Depth is required")
+        .isFloat({ min: 0 })
+        .withMessage("Depth must be positive"),
+    body("height")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Height is required")
+        .isFloat({ min: 0 })
+        .withMessage("Height must be positive"),
+    body("author", "Author is required")
+        .trim()
+        .isLength({ min: 1 }),
+    body("publicationDate", "Invalid publication date")
+        .isISO8601()
+        .toDate(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        const newBook = new Book({
+            title: req.body.title,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+            numberInStock: req.body.numberInStock,
+            pages: req.body.pages,
+            publisher: req.body.publisher,
+            isbn: req.body.isbn,
+            coverSize: [req.body.width, req.body.depth, req.body.height],
+            author: req.body.author,
+            publicationDate: req.body.publicationDate,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            async.parallel(
+                {
+                    authors(callback) {
+                        Author.find({})
+                            .sort({ name: 1 })
+                            .exec(callback);
+                    },
+                    categories(callback) {
+                        Category.find({})
+                            .sort({ name: 1 })
+                            .exec(callback);
+                    },
+                    publishers(callback) {
+                        Publisher.find({})
+                            .sort({ name: 1 })
+                            .exec(callback);
+                    }
+                },
+                (err, results) => {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    res.render("book_form", {
+                        title: "Create book",
+                        logoURL: "../../../images/amasonLogo.png",
+                        book: newBook,
+                        authors: results.authors,
+                        categories: results.categories,
+                        publishers: results.publishers,
+                        errors: errors.array(),
+                    });
+                }
+            );
+            return;
+        }
+        else {
+            // no errors
+            Book.findByIdAndUpdate(req.params.id, newBook, {}, (err, oldBook) => {
+                if (err) {
+                    return next(err);
+                }
+
+                res.redirect(oldBook.url);
+            });
+        }
+    }
+]
